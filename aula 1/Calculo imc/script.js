62e99b1f673c9ff6a92cf20f45a481ffb5145add
wandrey aguiar <wandreyaguiar0112@gmail.com>
2024-07-23 14:48:51 -0300

    alterações

--- aula 1/Calculo imc/script.js
+++ aula 1/Calculo imc/script.js
@@ -1,15 +1,18 @@
 window.onload = function () {
   document.getElementById("altura").addEventListener("focusout", function () {
-
+// acessando o index para pegar as entradas do usuario e tranformalas em variaveis 
     const peso = parseFloat(document.getElementById("peso").value);
     const altura = parseFloat(document.getElementById("altura").value);
     const resultado = document.getElementById("resultado");
     const mensagem = document.getElementById("mensagem");
 
+    // if para que o usuaria ensira valores maiores que 0
     if (!isNaN(peso) && !isNaN(altura) && altura > 0) {
       const imc = peso / (altura * altura);
       resultado.value = imc.toFixed(2);
-
+    // operação para obter o imc
+      
+    // condições do imc para escrever as mensagem sobre o peso
       if (imc < 18.5) {
         mensagem.textContent = "Você está abaixo do peso!!";
       } else if (imc >= 18.5 && imc < 25) {
